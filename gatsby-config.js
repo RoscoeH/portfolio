d76ef3545de455ffb128972e2d30f6764cded6ed
Roscoe Hart <roscoewh@gmail.com>
2021-06-03 16:41:02 +1000

    Add gatsby-plugin-theme-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `roscoe`,
+    description: `Web developer based in Australia. I like using the latest tech to make website, webapps and gizmos.`,
+    author: `@roscoeh`,
   },
   plugins: [
+    `gatsby-plugin-theme-ui`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
