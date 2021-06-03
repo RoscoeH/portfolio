08d92d872988a9ba12580f4b3ca977e29a45c497
Roscoe Hart <roscoewh@gmail.com>
2021-06-03 18:26:06 +1000

    Add mdx support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,21 @@ module.exports = {
     author: `@roscoeh`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/Layout.js"),
+        },
+      },
+    },
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
